b74d9dcb4e2e818ac62fb1deb9237282d029f957
kkavinkk <gk.kavin05@gmail.com>
2025-01-14 19:51:35 -0700

    Made a proper error code for invalid names app is now mostly complete

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const  response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
+    
 
-
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
+    
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
@@ -38,6 +43,10 @@ async function checkWeather(city) {
     }
 
     document.querySelector(".weather").style.display = "block"
+    document.querySelector(".error").style.display = "none";
+    }
+
+    
 }
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
